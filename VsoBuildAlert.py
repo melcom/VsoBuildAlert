49214aa09eba5426e57b7ea15252d9db6a899cd2
melcom <pierrehenri.gache@outlook.com>
2015-09-16 21:31:09 +0000

    add unicorn hat

--- VsoBuildAlert.py
+++ VsoBuildAlert.py
@@ -1,10 +1,19 @@
 ﻿from azure.servicebus import ServiceBusService, Message, Queue
 import json
+import unicornhat
 
 def build_succeeded():
+    for x in range(8):
+        for y in range(8):
+            unicornhat.set_pixel(x,y,0,255,0)
+    unicornhat.show()
     print('succeeded')
 
 def build_failed():
+    for x in range(8):
+        for y in range(8):
+            unicornhat.set_pixel(x,y,255,0,0)
+    unicornhat.show()
     print('failed')
 
 bus_service = ServiceBusService(
@@ -13,7 +22,7 @@ bus_service = ServiceBusService(
     shared_access_key_value='F4ALNAFvq7RqJhspB1AbXk/q2fZ7gedegN2rCYB9ehk=')
 
 while(True):
-    msg = bus_service.receive_queue_message('buildqueue', peek_lock=False, timeout=5)
+    msg = bus_service.read_delete_queue_message('buildqueue')
    
     if msg.body is None:
         print('No new message')

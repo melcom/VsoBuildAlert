1681ea51dc0b7d64ac737fad5d2d9d0ecd26b841
melcom <pierrehenri.gache@outlook.com>
2015-11-11 23:18:09 +0000

    change queue

--- VsoBuildAlert.py
+++ VsoBuildAlert.py
@@ -5,29 +5,30 @@ import unicornhat
 def build_succeeded():
     for x in range(8):
         for y in range(8):
-            unicornhat.set_pixel(x,y,0,255,0)
+            unicornhat.set_pixel(x,y,0,100,0)
     unicornhat.show()
     print('succeeded')
 
 def build_failed():
     for x in range(8):
         for y in range(8):
-            unicornhat.set_pixel(x,y,255,0,0)
+            unicornhat.set_pixel(x,y,100,0,0)
     unicornhat.show()
     print('failed')
 
 bus_service = ServiceBusService(
-    service_namespace='buildvsomessagerie',
-    shared_access_key_name='sasStrategy',
-    shared_access_key_value='F4ALNAFvq7RqJhspB1AbXk/q2fZ7gedegN2rCYB9ehk=')
+    service_namespace='vsobuildalert',
+    shared_access_key_name='listen',
+    shared_access_key_value='72rKCQ01AV+Cjvx1ee7qYVcWrwa3NJ8JO2tKY+M53+A=')
 
 while(True):
-    msg = bus_service.read_delete_queue_message('buildqueue')
+    msg = bus_service.read_delete_queue_message('build')
    
     if msg.body is None:
         print('No new message')
     else:
-        data = json.loads(msg.body.decode("utf-8"))
+        print(msg.body)
+        data = json.loads(msg.body.decode('utf-8'))
         if(data['resource']['status'] == 'succeeded'):
             build_succeeded()
         else:
